2f9ccd610b94c8af7a2e0fc8fee58f9d9b8b1252
Jonaxx8 <20r21a0543@mlrinstitutions.ac.in>
2022-11-10 18:44:54 +0530

    added shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
-
+import ShopPage from './pages/shop/shop.component';
+import { Route, Switch } from 'react-router';
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
