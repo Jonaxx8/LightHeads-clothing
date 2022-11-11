102f9d182aa571089395de8a11c1ff012907d410
Jonaxx8 <20r21a0543@mlrinstitutions.ac.in>
2022-11-11 21:48:18 +0530

    updated shop page with preview component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 
+import CollectionItem from "../collection-item/collection-item.component.jsx";
+
 import './collection-preview.styles.scss';
 
 const CollectionPreview = ({title, items}) => (
-    <div>
+    <div className="collection-preview">
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
             {
-                items.map(item => (
-                    <div key={item.id}>{item.name}</div>
+                items
+                .filter((item, idx)=> idx < 4)
+                .map(({id, ...otherItemProps})=> (
+                    <CollectionItem key={id} {...otherItemProps} />
                 ))
             }
         </div>
     </div>
-)
+);
 
 export default CollectionPreview;
