6124cc90b7e5d7acf44cc63e2bbd4d357e31f498
Jonaxx8 <20r21a0543@mlrinstitutions.ac.in>
2022-11-30 16:28:04 +0530

    updated shop data, updated cart utils to group items in cart reducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otehrProps }) => (
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otehrProps }) => (
     <button 
-    className={`${isGoogleSignIn ? 'google-sign-in': ''} custom-button`} 
+    className={`${inverted ? 'inverted': ''} ${isGoogleSignIn ? 'google-sign-in': ''} custom-button`} 
     {...otehrProps}>
         {children}
     </button>
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,5 +1,11 @@
 import CartActionTypes from "./cart.types";
 
 export const toggleCartHidden = () => ({
-    type: CartActionTypes.TOGGLE_CART_HIDDEN
+    type: CartActionTypes.TOGGLE_CART_HIDDEN,
+    ADD_ITEM: 'ADD_ITEM'
+});
+
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
 });
